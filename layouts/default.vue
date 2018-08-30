13854153d0d2f6b41279fbd715e0f8aaff03ae73
Infant Frontender <newbornfrontender@gmail.com>
2018-08-30 10:59:12 +0300

    Добавил тестовую страницу и макет

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="main">
+    <header>
+      <ul>
+        <li>
+          <nuxt-link to="/">Home page</nuxt-link>
+        </li>
+      </ul>
+    </header>
+    <main>
+      <nuxt/>
+    </main>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+console.log('123');
+const test = '1'
+test = '2'
+
+export default {
+  data: () => ({
+    msg: 'Nuxt',
+  }),
+};
+</script>
