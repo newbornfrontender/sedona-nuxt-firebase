b077c56c93311cd06fcdcadf8a7c67ae1cfc1171
Infant Frontender <newbornfrontender@gmail.com>
2018-09-02 11:45:33 +0300

    Добавил страницы с тестовым кодом

---
+++ pages/gallery.vue
@@ -0,0 +1,32 @@
+<template>
+  <section>
+    <div>{{ msg }}</div>
+    <div :class="$style.testStyle">Test</div>
+  </section>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    msg: 'Фото и видео',
+  }),
+  head() {
+    return {
+      title: this.msg,
+      meta: [
+        {
+          hid: 'gallery-description',
+          name: 'description',
+          content: this.msg,
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style module>
+.testStyle {
+  color: red;
+}
+</style>
---
+++ pages/review-form.vue
@@ -0,0 +1,23 @@
+<template>
+  <section>
+    <div>{{ msg }}</div>
+    <!-- <div>Current MQ: {{ $mq }}</div> -->
+    <!-- <div v-if="$mq === 'sm'">SM with "$mq === 'sm'"</div> -->
+    <mq-layout mq="xs">
+      <span> Display on md and larger </span>
+    </mq-layout>
+    <!-- <% if(debug) { %>
+      <p class="description">Debug</p>
+    <% } %> -->
+  </section>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    msg: 'Форма отзыва',
+  }),
+  // <% if(process.browser) { %>
+  // <% }, %>
+};
+</script>
