7cc9a3dd0087588467219c91d826d30530e4d439
Infant Frontender <newbornfrontender@gmail.com>
2018-09-02 11:47:09 +0300

    Добавил шаблон ошибки

---
+++ layouts/error.vue
@@ -0,0 +1,18 @@
+<template>
+  <section>
+    <h1 v-if="error.statusCode === 404">Страница не найдена</h1>
+    <h1 v-else>Ошибка</h1>
+    <nuxt-link to="/">Главная</nuxt-link>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    error: {
+      type: Object,
+      default: () => {},
+    },
+  },
+};
+</script>
