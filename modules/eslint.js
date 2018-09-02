69b5320ef62a23c112f6b0e47c072c03470783bf
Infant Frontender <newbornfrontender@gmail.com>
2018-09-02 10:05:31 +0300

    Убрал закомментированный код

--- modules/eslint.js
+++ modules/eslint.js
@@ -1,22 +1,3 @@
-// /* eslint-disable-next-line func-names */
-// module.exports = function() {
-//   this.nuxt.hook('build', async (builder) => {
-//     /* eslint-disable-next-line no-unused-vars */
-//     builder.hook('compile', ({ compiler }) => {
-//       this.extendBuild((config, ctx) => {
-//         if (ctx.isDev && ctx.isClient) {
-//           config.module.rules.push({
-//             enforce: 'pre',
-//             test: /\.(js(x)|vue)$/,
-//             use: 'eslint-loader',
-//             exclude: /node_modules/,
-//           });
-//         }
-//       });
-//     });
-//   });
-// };
-
 /* eslint-disable-next-line func-names */
 module.exports = function() {
   this.extendBuild((config, ctx) => {
