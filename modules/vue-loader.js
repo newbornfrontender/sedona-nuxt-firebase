6742ead0ca0ea6bea6a1b9b84ed864f5a710bb4a
Infant Frontender <newbornfrontender@gmail.com>
2018-09-02 11:42:05 +0300

    Убрал закомментированный код

--- modules/vue-loader.js
+++ modules/vue-loader.js
@@ -11,21 +11,3 @@ module.exports = function() {
     };
   });
 };
-
-// /* eslint-disable-next-line func-names */
-// module.exports = function() {
-//   this.nuxt.hook('build', async (builder) => {
-//     builder.hook('compile', () => {
-//       this.extendBuild((config) => {
-//         const vueLoader = config.module.rules.find(
-//           (rule) => rule.loader === 'vue-loader',
-//         );
-
-//         vueLoader.options.cssModules = {
-//           localIdentName: '[path]-[name]--[local]__[hash:base64:5]_',
-//           camelCase: 'only',
-//         };
-//       });
-//     });
-//   });
-// };
